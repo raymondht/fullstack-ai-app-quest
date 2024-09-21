423856afbbf4f3bc700d5590e085c1d14af3de0d
Raymond Hieu Tran <raymondhieutran@gmail.com>
2024-09-21 13:03:22 +1000

    remove headless configuration

--- apps/front-end/src/tests/config/playwright.config.ts
+++ apps/front-end/src/tests/config/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     reuseExistingServer: true
   },
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     actionTimeout: 0,
     ignoreHTTPSErrors: true,
